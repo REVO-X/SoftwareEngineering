6852d9ee9b814130edd301533e824b19fde5606d
Zorigt Batzorigt <s1931701oh@s.chibakoudai.jp>
2022-06-15 23:40:59 +0900

    first commit

---
+++ SE/test.c
@@ -0,0 +1,41 @@
+#include <assert.h>
+#include <stdio.h>
+int collatz(int n){
+int i = 0;
+while(n>1){
+        if(n % 2 == 00){
+                n /= 2;
+                i++;
+        } else{
+                n = 3*n + 1;
+                i++;
+        }
+}
+return i;
+}
+
+int main(void)
+{
+//テストケースを配列に入れる {引数，戻り値，成否}
+int sample[3][3]=
+  {{2, 1, 0},
+   {-1,0,-1},
+   {3, 7, 0}
+  };
+//考えて書く
+ int i, z, x, y;
+ for(i=0; i<3; i++){
+   x = sample[i][0];
+   y = collatz(x);
+   assert(y == sample[i][1]);
+   if(y != 0){
+     z = 0;
+   }
+   else{
+     z = -1;
+   }
+   assert(z == sample[i][2]);
+   printf("%d %d %d\n", x, y, z);
+ }
+ return 0;
+}
